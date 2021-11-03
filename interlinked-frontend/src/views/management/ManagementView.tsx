c2253b9ddf2576e0b5caf09cf97f9ad0d0a10863
AldoPC <aldoponcedelacruz@gmail.com>
2021-11-02 20:40:51 -0600

    unfinished management view
    missing css and validation

--- interlinked-frontend/src/views/management/ManagementView.tsx
+++ interlinked-frontend/src/views/management/ManagementView.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 const ManagementView: React.FC = () => {
   return (
     <div>
-      <h1>Management View</h1>
+      <h1>Publicar nuevo video</h1>
+      <form>
+        <Stack spacing={1} sx={{ width: 300 }}>
+          <TextField id="outlined-title-input" label="Titulo" />
+          <TextField id="outlined-link-input" label="Link" />
+          <TextField  id="outlined-multiline"
+          label="Descripción"
+          multiline
+          rows={5}/>
+          <Button variant="contained" disableElevation>
+            Publicar video
+          </Button>
+          <Button variant="contained" color="error">
+            Cancelar
+          </Button>
+        </Stack>
+      </form>
     </div>
   );
 };
