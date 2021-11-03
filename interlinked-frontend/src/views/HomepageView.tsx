28c8e30fcf18bf333433033d1a78957708065418
Samuel2779 <44899739+Samuel2779@users.noreply.github.com>
2021-11-03 13:25:02 -0600

    update homepageview

--- interlinked-frontend/src/views/HomepageView.tsx
+++ interlinked-frontend/src/views/HomepageView.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+/** @jsxImportSource @emotion/react */ import { css } from '@emotion/react';
+
+const backgroundImage = css({
+  width: '100%',
+  backgroundImage: `url("https://c.tenor.com/8gGCkmG_slAAAAAd/destiny2-beyond-light.gif")` ,
+  backgroundColor: 'black',
+  minHeight: 'calc(100vh - 96px)',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
+  backgroundSize: '95% 95%',
+  color: 'white',
+  justifyContent: 'center',
+  display: 'flex',
+  alignItems: 'center',
+});
 
 const HomepageView: React.FC = () => {
   return (
-    <div>
-      <h1>FixWithKors Gaming</h1>
-      <p> Description</p>
+    <div css={backgroundImage}>
+      <div>
+        <h1>FixWithKors Gaming</h1>
+        <p> Description</p>
+      </div>
     </div>
   );
 };
