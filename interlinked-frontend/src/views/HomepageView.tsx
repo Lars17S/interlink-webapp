6dc87537d5e8c335771d67c026b6392e1734cc92
Samuel2779 <44899739+Samuel2779@users.noreply.github.com>
2021-11-03 20:27:42 -0600

    Fixed homepageVideo

--- interlinked-frontend/src/views/HomepageView.tsx
+++ interlinked-frontend/src/views/HomepageView.tsx
@@ -1,23 +1,35 @@
 import React from 'react';
 /** @jsxImportSource @emotion/react */ import { css } from '@emotion/react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const backgroundImage = css({
   width: '100%',
   backgroundImage: `url("https://c.tenor.com/8gGCkmG_slAAAAAd/destiny2-beyond-light.gif")` ,
   backgroundColor: 'black',
-  minHeight: 'calc(100vh - 96px)',
+  minHeight: 'calc(100vh)',
   backgroundRepeat: 'no-repeat',
   backgroundPosition: 'center',
-  backgroundSize: '95% 95%',
+  backgroundSize: '100%',
   color: 'white',
   justifyContent: 'center',
   display: 'flex',
   alignItems: 'center',
+
   
+});
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      default: '#000000',
+      paper: '#121212',
+    },
+  },
 });
 
 const blockValues = css({
   width:'30%',
+  
   
 });
 const blockParagraph = css({
@@ -27,14 +39,16 @@ const blockParagraph = css({
 });
 const HomepageView: React.FC = () => {
   return (
-    <div css={backgroundImage}>
-      <div css={blockValues}>
-        <h1>FixWithKors Gaming</h1>
-        <p css={blockParagraph}> FixWithKors es una empresa que se enfoca en los amantes del Gaming, podras disfrutar de distintos
-          videos relacionados con juegos de Shooters, MOBAs, deportes o RPGs. Disfruta mucho de la aventura.
-        </p>
+    <ThemeProvider theme={theme}>
+      <div css={backgroundImage}>
+        <div css={blockValues}>
+          <h1>FixWithKors Gaming</h1>
+          <p css={blockParagraph}> FixWithKors es una empresa que se enfoca en los amantes del Gaming, podras disfrutar de distintos
+            videos relacionados con juegos de Shooters, MOBAs, deportes o RPGs. Disfruta mucho de la aventura.
+          </p>
+        </div>
       </div>
-    </div>
+     </ThemeProvider>
   );
 };
 
--- interlinked-frontend/src/views/about-us/AboutUsView.tsx
+++ interlinked-frontend/src/views/about-us/AboutUsView.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-//import FacebookIcon from '@mui/icons-material/Facebook';
+import FacebookIcon from '@mui/icons-material/Facebook';
+/** @jsxImportSource @emotion/react */ import { css } from '@emotion/react';
+
+const backgroundDesign = css({
+  backgroundColor: 'black',
+  color: 'white',
+});
 
 const AboutUsView: React.FC = () => {
   return (
-    <div>
+    <div css={backgroundDesign}>
       <h2>FixWithKors Gaming</h2>
       <p>Descripcion de la empresa</p>
-      {/* <FacebookIcon /> */}
+      <FacebookIcon />
     </div>
   );
 };
